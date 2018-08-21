b8a9a8652b58fc39ed3cc501ea3eefba115a97af
Ville Ristimäki <villeristimaki@gmail.com>
2018-08-21 13:29:40 +0300

    Update kokoelmat modules

---
+++ osa-2/01-kokoelmien-renderointi/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import Kurssi from './kurssi'
+
+const App = () => {
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      <h1>Opetusohjelma</h1>
+      {kurssit.map((kurssi) => <Kurssi key={kurssi.id} kurssi={kurssi} />)}
+    </div>
+  )
+}
+
+export default App;
--- osa-2/01-kokoelmien-renderointi/src/index.js
+++ osa-2/01-kokoelmien-renderointi/src/index.js
@@ -1,58 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Kurssi from './kurssi'
+import App from './App';
 
-const App = () => {
-  const kurssit = [
-    {
-      nimi: 'Half Stack -sovelluskehitys',
-      id: 1,
-      osat: [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
-        }
-      ]
-    },
-    {
-      nimi: 'Node.js',
-      id: 2,
-      osat: [
-        {
-          nimi: 'Routing',
-          tehtavia: 3,
-          id: 1
-        },
-        {
-          nimi: 'Middlewaret',
-          tehtavia: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-  return (
-    <div>
-      <h1>Opetusohjelma</h1>
-      {kurssit.map((kurssi) => <Kurssi key={kurssi.id} kurssi={kurssi} />)}
-    </div>
-  )
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
