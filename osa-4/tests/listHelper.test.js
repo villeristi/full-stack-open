402ea00f14a58a571730d9b7da557f49e49eebac
Ville Ristimäki <villeristimaki@gmail.com>
2018-08-23 00:06:51 +0300

    Add own describe-block

--- osa-4/tests/listHelper.test.js
+++ osa-4/tests/listHelper.test.js
@@ -1,31 +1,33 @@
 const listHelper = require('./util/listHelper')
 const dummyBlogs = require('./util/blogs')
 
-describe('total likes', () => {
-  test('Calculate total likes of all blogs', () => {
-    const result = listHelper.totalLikes(dummyBlogs)
-    expect(result).toBe(36)
-  })
-})
-
-describe('Most likes', () => {
-  test('Finds the blog with most likes', () => {
-    const mostLikedTitle = 'Canonical string reduction'
-
-    expect(listHelper.favoriteBlog(dummyBlogs).title).toEqual(mostLikedTitle)
+describe('List helpers', () => {
+  describe('total likes', () => {
+    test('Calculate total likes of all blogs', () => {
+      const result = listHelper.totalLikes(dummyBlogs)
+      expect(result).toBe(36)
+    })
   })
 
-  test('Finds the author with most likes', () => {
-    const mostLikedAuthor = { author: 'Edsger W. Dijkstra', likes: 12 }
+  describe('Most likes', () => {
+    test('Finds the blog with most likes', () => {
+      const mostLikedTitle = 'Canonical string reduction'
 
-    expect(listHelper.mostLikes(dummyBlogs)).toEqual(mostLikedAuthor)
+      expect(listHelper.favoriteBlog(dummyBlogs).title).toEqual(mostLikedTitle)
+    })
+
+    test('Finds the author with most likes', () => {
+      const mostLikedAuthor = { author: 'Edsger W. Dijkstra', likes: 12 }
+
+      expect(listHelper.mostLikes(dummyBlogs)).toEqual(mostLikedAuthor)
+    })
   })
-})
 
-describe('Most active', () => {
-  test('Finds the author with most blogs', () => {
-    const mostActive = { author: 'Robert C. Martin', blogCount: 3 }
+  describe('Most active', () => {
+    test('Finds the author with most blogs', () => {
+      const mostActive = { author: 'Robert C. Martin', blogCount: 3 }
 
-    expect(listHelper.mostBlogs(dummyBlogs)).toEqual(mostActive)
+      expect(listHelper.mostBlogs(dummyBlogs)).toEqual(mostActive)
+    })
   })
 })
