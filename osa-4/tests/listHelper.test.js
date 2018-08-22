e29080b51183790ad50ac81e92b2b8f5dffc1b0b
Ville Ristimäki <villeristimaki@gmail.com>
2018-08-22 23:14:23 +0300

    4.5

--- osa-4/tests/listHelper.test.js
+++ osa-4/tests/listHelper.test.js
@@ -1,19 +1,17 @@
 const listHelper = require('./util/listHelper')
+const dummyBlogs = require('./util/blogs')
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    },
-  ]
+  test('Calculate total likes of all blogs', () => {
+    const result = listHelper.totalLikes(dummyBlogs)
+    expect(result).toBe(36)
+  })
+})
 
-  test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
+describe('Most likes', () => {
+  test('Finds the blog with most likes', () => {
+    const mostLikedTitle = 'Canonical string reduction'
+
+    expect(listHelper.favoriteBlog(dummyBlogs).title).toEqual(mostLikedTitle)
   })
 })
--- osa-4/tests/util/listHelper.js
+++ osa-4/tests/util/listHelper.js
@@ -1,5 +1,9 @@
 module.exports = {
   totalLikes(blogData = []) {
     return blogData.reduce((a, { likes }) => a + likes, 0)
+  },
+
+  favoriteBlog(blogData = []) {
+    return blogData.reduce((a, b) => b.likes > a.likes ? b : a, blogData[0])
   }
 }
