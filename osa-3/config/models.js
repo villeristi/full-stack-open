9f2315c1b469802283b1a57a68f2db0289e8f9e1
Ville Ristimäki <villeristimaki@gmail.com>
2018-08-22 08:57:49 +0300

    Add validation to PersonSchema

--- osa-3/config/models.js
+++ osa-3/config/models.js
@@ -9,9 +9,27 @@ const PersonSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+}, {
+  // Format responses
+  toJSON: {
+    transform(doc, {_id, name, number}) {
+      return {
+        id: _id,
+        name,
+        number
+      }
+    }
+  }
 })
 
-PersonSchema.method({})
-PersonSchema.statics = {}
+PersonSchema.path('name').validate({
+  async validator(name) {
+    const count = await this.model('Person').count({ name })
+    return count === 0
+  },
+
+  message: 'Nimi `{VALUE}` löytyy jo tietokannasta!'
+});
+
 
 module.exports = mongoose.model('Person', PersonSchema)
